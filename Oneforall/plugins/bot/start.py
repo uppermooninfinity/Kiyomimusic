a19444fa578a32ef8645d57b133e0fdfc03c35e3
ARJUN <pachangedinanath@gmail.com>
2026-01-17 17:41:51 +0530

    Update start.py

--- Oneforall/plugins/bot/start.py
+++ Oneforall/plugins/bot/start.py
@@ -35,9 +35,11 @@ from Oneforall.misc import SUDOERS
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
     await message.react("❤")
+    loading = await message.reply_text("<b>ʟᴏᴀᴅɪɴɢ...</b>")
+    await asyncio.sleep(0.5)
+    await loading.delete
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
-
         if name[0:3] == "del":
             await del_plist_msg(client=client, message=message, _=_)
             
