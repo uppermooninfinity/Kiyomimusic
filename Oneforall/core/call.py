3b58381aa2474816bc9425030d040ffa478cda45
UCHIHABOTS <txkuze@gmail.com>
2026-02-01 12:04:08 +0530

    Update call.py

--- Oneforall/core/call.py
+++ Oneforall/core/call.py
@@ -1,41 +1,4 @@
-import asyncio
-import os
-from datetime import datetime, timedelta
-from typing import Union
 
-from ntgcalls import TelegramServerError
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
-from pytgcalls.types import AudioQuality, MediaStream, Update, VideoQuality
-from pytgcalls.types.stream import StreamAudioEnded
-
-import config
-from Oneforall import LOGGER, YouTube, app
-from Oneforall.misc import db
-from Oneforall.utils.database import (
-    add_active_chat,
-    add_active_video_chat,
-    get_lang,
-    get_loop,
-    group_assistant,
-    is_autoend,
-    music_on,
-    remove_active_chat,
-    remove_active_video_chat,
-    set_loop,
-)
-from Oneforall.utils.exceptions import AssistantErr
-from Oneforall.utils.formatters import check_duration, seconds_to_min, speed_converter
-from Oneforall.utils.inline.play import stream_markup, stream_markup2
-from Oneforall.utils.stream.autoclear import auto_clean
-from Oneforall.utils.thumbnails import get_thumb
-from strings import get_string
-
-autoend = {}
-counter = {}
-loop = asyncio.get_event_loop_policy().get_event_loop()
 
 
 async def _clear_(chat_id):
@@ -348,9 +311,8 @@ class Call(PyTgCalls):
                 autoend[chat_id] = datetime.now() + timedelta(minutes=1)
 
 
-    async def change_stream(self, client: PyTgCalls, chat_id: int):
-    await delete_old_message(chat_id)
-
+     async def change_stream(self, client: PyTgCalls, chat_id: int):
+       await delete_old_message(chat_id)
     check = db.get(chat_id)
     popped = None
     loop = await get_loop(chat_id)
