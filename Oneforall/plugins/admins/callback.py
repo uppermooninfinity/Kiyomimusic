a7634073262ac0ea896cf876321c1492ae61b0e9
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-23 11:30:10 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/plugins/admins/callback.py
+++ Oneforall/plugins/admins/callback.py
@@ -194,7 +194,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             await CallbackQuery.edit_message_reply_markup(
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
-        except
+        except:
         pass
         await CallbackQuery.message.delete()
     elif command == "Skip" or command == "Replay":
