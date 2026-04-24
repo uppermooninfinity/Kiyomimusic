a7a26092181055bee27f95f6ac80b24f37e40c7c
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-24 12:39:25 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/utils/inline/play.py
+++ Oneforall/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="💿", callback_data=f"ADMIN Replay|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
