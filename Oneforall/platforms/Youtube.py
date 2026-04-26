580dfccffb2361488d8219f9a58b79cb6ab88bde
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-26 23:25:15 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/platforms/Youtube.py
+++ Oneforall/platforms/Youtube.py
@@ -9,7 +9,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 from Oneforall.utils.database import is_on_off
 from Oneforall.utils.formatters import time_to_seconds
