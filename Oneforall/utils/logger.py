df6fbbd90b54f670e7cefb9aa1d4eb0d09ec0b4e
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-08 10:22:12 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/utils/logger.py
+++ Oneforall/utils/logger.py
@@ -1,38 +1,50 @@
 from pyrogram.enums import ParseMode
+
 from Oneforall import app
 from Oneforall.utils.database import is_on_off
 from config import LOGGER_ID
 
+LOGGER_PIC = "https://graph.org/file/63b3df811a74a74849d45-f46859bfc348595942.jpg"  # put your image link here
+
+
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        logger_text = f"""<blockquote>
-<b>ᴍᴜsɪᴄ ʀᴇᴄᴏʀᴅs</b>
 
-<b>• ɪɴǫᴜɪʀʏ ⌯ </b> {message.text.split(None, 1)[1]}
-<b>• sᴏᴜʀᴄᴇ ⌯ </b> {streamtype}
+        query = (
+            message.text.split(None, 1)[1]
+            if len(message.text.split()) > 1
+            else "Unknown"
+        )
 
-<b>ᴀʙᴏᴜᴛ ᴄʜᴀᴛ - </b> 
+        logger_text = f"""
+<b>➻ 𝛂 ηєᴡ ᴧᴄᴛɪᴠɪᴛʏ ɪѕ ᴅєᴛєᴄᴛєᴅ 🎵</b>
 
-<b>↬ ᴅᴇsɪɢɴᴀᴛɪᴏɴ ⌯ </b> {message.chat.title}
-<b>↬ ɪᴅᴇɴᴛɪғɪᴇʀ ⌯ </b> <code>{message.chat.id}</code>
-<b>↬ ᴄʜᴀᴛ ʜᴀɴᴅʟᴇ ⌯ </b> @{message.chat.username}
+<b>⌬ ᴄʜᴧᴛ</b>
+<b>• ɴᴧϻє :</b> {message.chat.title}
+<b>• ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>• ᴜѕєʀɴᴧϻє :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ᴅᴀᴛᴀ - </b>
+<b>⌬ ᴜѕєʀ</b>
+<b>• ɴᴧϻє :</b> {message.from_user.mention}
+<b>• ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>• ᴜѕєʀɴᴧϻє :</b> @{message.from_user.username}
 
-<b>↬ ɴᴀᴍᴇ ⌯ </b> {message.from_user.mention}
-<b>↬ ɪᴅᴇɴᴛɪғɪᴇʀ ⌯ </b> <code>{message.from_user.id}</code>
-<b>↬ ʜᴀɴᴅʟᴇ ⌯ </b> @{message.from_user.username}<blockquote>"""
+<b>⌬ ѕᴛʀєᴧϻ</b>
+<b>• ᴛʏᴘє :</b> {streamtype}
+<b>• ǫᴜєʀʏ :</b> <code>{query}</code>
 
-        # Send log to the primary log channel
+<b>⌬ ʙʏ :</b> {app.mention}
+"""
+
         if message.chat.id != LOGGER_ID:
             try:
-                await app.send_message(
+                await app.send_photo(
                     chat_id=LOGGER_ID,
-                    text=logger_text,
+                    photo=LOGGER_PIC,
+                    caption=logger_text,
                     parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
                 )
-            except Exception as e:
-                print(f"Failed to send log to LOGGER_ID: {e}")
+            except:
+                pass
 
-        
+        return
