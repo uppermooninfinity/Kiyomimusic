d19ededd6f7a5d3c4fd59aceaee8670197fa126f
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-24 22:41:33 +0530

    Update Youtube.py

--- Oneforall/platforms/Youtube.py
+++ Oneforall/platforms/Youtube.py
@@ -10,7 +10,7 @@ import aiohttp
 
 API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsaA5ST160vHMqqU3HP0Bt") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
